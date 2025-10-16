b1ec9d068c795b7a122d13da5b7867b61982ba77
03AJ03 <203957546+03AJ03@users.noreply.github.com>
2025-10-16 09:06:04 +0000

    HackerRank submission: 451298718 - Interviews

--- HackerRank/SQL/Interviews/solution.sql
+++ HackerRank/SQL/Interviews/solution.sql
@@ -1,6 +1,32 @@
 /*
 Enter your query here.
 */
+-- SELECT 
+--     c.contest_id,
+--     c.hacker_id,
+--     c.name,
+--     SUM(s.total_submissions) AS total_submissions,
+--     SUM(s.total_accepted_submissions) AS total_accepted_submissions,
+--     SUM(v.total_views) AS total_views,
+--     SUM(v.total_unique_views) AS total_unique_views
+-- FROM Contests c
+-- JOIN Colleges col 
+--     ON c.contest_id = col.contest_id
+-- JOIN Challenges ch 
+--     ON col.college_id = ch.college_id
+-- LEFT JOIN View_Stats v 
+--     ON ch.challenge_id = v.challenge_id
+-- LEFT JOIN Submission_Stats s 
+--     ON ch.challenge_id = s.challenge_id
+-- GROUP BY 
+--     c.contest_id, c.hacker_id, c.name
+-- HAVING 
+--     SUM(s.total_submissions) > 0 OR
+--     SUM(s.total_accepted_submissions) > 0 OR
+--     SUM(v.total_views) > 0 OR
+--     SUM(v.total_unique_views) > 0
+-- ORDER BY 
+--     c.contest_id;
 SELECT 
     con.contest_id,
     con.hacker_id,
